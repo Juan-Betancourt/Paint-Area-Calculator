030d57713fc26c3113f133c244497047c9574b10
Juan Betancourt <juancbetancourt11@gmail.com>
2022-05-10 13:35:15 +0000

    Completed the challenge with the use of def, functions, and parameters.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+import math
 #Write your code below this line 👇
-
-
-
-
-
+def paint_calc(height, width, cover):
+  area = height * width
+  num_of_cans = math.ceil(area / cover)
+  print(f"You will need {num_of_cans} cans of paint")
 
 
 #Write your code above this line 👆
@@ -12,7 +12,7 @@
 # 🚨 Don't change the code below 👇
 test_h = int(input("Height of wall: "))
 test_w = int(input("Width of wall: "))
-coverage = 5
+coverage = 5 
 paint_calc(height=test_h, width=test_w, cover=coverage)
 
 
